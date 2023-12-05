cf5cfd1cc5f3aa74ef64154a723373095688fd79
Leandro Ferreira <leandro.wsp@gmail.com>
2023-12-05 16:52:44 -0300

    preparando para jpa

--- src/main/java/io/github/dougllasfps/repositorio/Clientes.java
+++ src/main/java/io/github/dougllasfps/repositorio/Clientes.java
@@ -14,59 +14,7 @@ import java.util.List;
 @Repository
 public class Clientes {
 
-    private static String INSERT = "insert into cliente (nome) values (?) ";
-    private static String SELECT_ALL = "SELECT * FROM CLIENTE ";
-    private static String UPDATE = "update cliente set nome = ? where id = ? ";
-    private static String DELETE = "delete from cliente where id = ? ";
 
-    private static String DELETE_INICICIAS = "delete from cliente where nome like ?";
 
-    @Autowired
-    private JdbcTemplate jdbcTemplate;
-
-    public Cliente salvar(Cliente cliente){
-        jdbcTemplate.update( INSERT, new Object[]{cliente.getNome()} );
-        return cliente;
-    }
-
-    public Cliente atualizar(Cliente cliente){
-        jdbcTemplate.update(UPDATE, new Object[]{
-                cliente.getNome(), cliente.getId()} );
-        return cliente;
-    }
-
-    public void deletar(Cliente cliente){
-        deletar(cliente.getId());
-    }
-
-    public void deletar(Integer id){
-        jdbcTemplate.update(DELETE, new Object[]{id});
-    }
-
-    public List<Cliente> buscarPorNome(String nome){
-        return jdbcTemplate.query(
-                SELECT_ALL.concat(" where nome like ? "),
-                new Object[]{"%" + nome + "%"},
-                obterClienteMapper());
-    }
-
-    public List<Cliente> obterTodos(){
-        return jdbcTemplate.query(SELECT_ALL, obterClienteMapper());
-    }
-
-    private RowMapper<Cliente> obterClienteMapper() {
-        return new RowMapper<Cliente>() {
-            @Override
-            public Cliente mapRow(ResultSet resultSet, int i) throws SQLException {
-                Integer id = resultSet.getInt("id");
-                String nome = resultSet.getString("nome");
-                return new Cliente(id, nome);
-            }
-        };
-    }
-
-    public void deletarPorNomeIniciandoCom(String inicioNome){
-        jdbcTemplate.update(DELETE_INICICIAS, inicioNome + "%");
-    }
 
 }
