1c125a251818c90369a797bcdc0e9443d35a0f44
Leandro Ferreira <leandro.wsp@gmail.com>
2023-12-13 16:47:29 -0300

    metodo get implementado

--- src/main/java/io/github/dougllasfps/VendasApplication.java
+++ src/main/java/io/github/dougllasfps/VendasApplication.java
@@ -1,17 +1,25 @@
 package io.github.dougllasfps;
 
+import io.github.dougllasfps.domain.Cliente;
+import io.github.dougllasfps.repository.Clientes;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-@RestController
 public class VendasApplication {
 
+    @Bean
+    public CommandLineRunner commandLineRunner(@Autowired Clientes clientes){
+        return args -> {
+            Cliente c = new Cliente(null, "Asa");
+            clientes.save(c);
+        };
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);
     }
-
-
 }
--- src/main/java/io/github/dougllasfps/rest/controller/ClienteController.java
+++ src/main/java/io/github/dougllasfps/rest/controller/ClienteController.java
@@ -1,19 +1,34 @@
 package io.github.dougllasfps.rest.controller;
 
-
+import io.github.dougllasfps.domain.Cliente;
+import io.github.dougllasfps.repository.Clientes;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Optional;
 
 @Controller
 public class ClienteController {
 
+    private Clientes clientes;
 
+    public ClienteController( Clientes clientes ) {
+        this.clientes = clientes;
+    }
 
+    @GetMapping("/api/clientes/{id}")
+    @ResponseBody
+    public ResponseEntity getClienteById( @PathVariable Integer id ){
+        Optional<Cliente> cliente = clientes.findById(id);
 
+        if(cliente.isPresent()){
+            return ResponseEntity.ok( cliente.get() );
+        }
 
-
-
-
-
-
+        return ResponseEntity.notFound().build();
+    }
 
 }
